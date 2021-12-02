9e1063daaebc967226dd7fc4477beced85686166
phiphi0912 <610733005@gms.ndhu.edu.tw>
2021-12-02 15:23:33 +0800

    feat: add checkValidity

--- routes/modules/shorten.js
+++ routes/modules/shorten.js
@@ -5,8 +5,9 @@ const shortenURL = require('../../shortenURL')
 
 router.post('/submit', (req, res) => {
   const originalURL = req.body.url
+  if (!originalURL) return
   //判斷資料庫有無輸入的原始網址，如果有拿出已儲存的網址返回，沒有則create一個新的
-  url.find({ originalURL: originalURL })
+  url.find({ originalURL })
     .lean()
     .then(shortener => {
       if (shortener.length === 0) {
